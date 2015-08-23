6a2285cbf12da6f48034f65962f5abd839d418b7
moroli <doudouli06@yahoo.fr>
2015-08-23 16:20:59 +0200

    new

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions that are used to invert a matrix and cache the result. Before any calculation,  
+## checks the presence of the result in the cache in order to avoir recomputation.
 
-## Write a short comment describing this function
+
+## makeCacheMatrix creates a special "Matrix", which is a list containing a function to
+##  1-set the value of the vector
+##  2-get the value of the vector
+##  3-set the value of the mean
+##  4-get the value of the mean
+## Check, prior to any calculation, if the matrix is invertible
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -35,7 +41,11 @@ makeCacheMatrix <- function(x = matrix()) {
   }
 }
 
-## Write a short comment describing this function
+## Calculates the inverse of the matrix created with the above function. 
+## Checks first to see if the inverse has already been calculated. 
+## If so, it gets the mean from the cache and skips the computation. 
+## Otherwise, it calculates the inverse of the matrix and sets the value 
+## of the inverse matrix in the cache via the SetMatInv function.
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
